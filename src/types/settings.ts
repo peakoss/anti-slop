b81c88b2da55e4de532c920b056f92bfe2b176e0
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-02-09 22:26:52 +0100

    feat(inputs): add all inputs to the settings type

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,4 +1,90 @@
 export interface Settings {
     // Repo Auth Token
     githubToken: string;
+
+    // General Settings
+    maxFailures: number;
+    actionsStepDebug: boolean;
+
+    // PR Branch Checks
+    allowedTargetBranches: string[];
+    blockedTargetBranches: string[];
+    allowedSourceBranches: string[];
+    blockedSourceBranches: string[];
+
+    // PR Title Checks
+    requireConventionalTitle: boolean;
+
+    // PR Description Checks
+    requireDescription: boolean;
+    maxDescriptionLength: number;
+    maxEmojiCount: number;
+    requirePrTemplate: boolean;
+    requireLinkedIssue: boolean;
+    blockedKeywords: string[];
+    blockedIssueNumbers: string[];
+
+    // Commit Message Checks
+    requireConventionalCommits: boolean;
+    blockedCommitAuthors: string[];
+
+    // File Checks
+    allowedFileExtensions: string[];
+    allowedPaths: string[];
+    blockedPaths: string[];
+    requireFinalNewline: boolean;
+    allowedLineEnding: string;
+
+    // User Health Checks
+    minRepoMergedPrs: number;
+    minRepoMergeRatio: number;
+    minGlobalMergeRatio: number;
+    minAccountAge: number;
+
+    // Filters
+    // onlyIssueTypes: string[];
+    // onlyLabels: string[];
+    // onlyPrLabels: string[];
+    // onlyIssueLabels: string[];
+    // anyOfLabels: string[];
+    // anyOfPrLabels: string[];
+    // anyOfIssueLabels: string[];
+
+    // Exemptions
+    exemptAuthorAssociation: string[];
+    // exemptTeams: string[];
+    exemptUsers: string[];
+    exemptBots: string[];
+    exemptDraftPrs: boolean;
+    exemptLabel: string;
+    exemptPrLabel: string;
+    // exemptIssueLabel: string;
+    // exemptMilestones: string[];
+    // exemptPrMilestones: string[];
+    // exemptIssueMilestones: string[];
+    // exemptAllMilestones: boolean;
+    // exemptAllPrMilestones: boolean;
+    // exemptAllIssueMilestones: boolean;
+
+    // PR Close Actions
+    closePr: boolean;
+    lockPr: boolean;
+    deleteBranch: boolean;
+
+    // PR Failure Actions
+    failurePrMessage: string;
+    failureRemovePrLabels: string[];
+    failureRemoveAllPrLabels: boolean;
+    failureAddPrLabels: string[];
+
+    // Issue Close Actions
+    // closeIssue: boolean;
+    // closeIssueReason: string;
+    // lockIssue: boolean;
+
+    // Issue Failure Actions
+    // failureIssueMessage: string;
+    // failureRemoveIssueLabels: string[];
+    // failureRemoveAllIssueLabels: boolean;
+    // failureAddIssueLabels: string[];
 }
