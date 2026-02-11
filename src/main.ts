12da2c05894f504625f1e071ca1b624f8f789280
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-02-11 14:10:19 +0100

    feat(inputs): get settings in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import * as core from "@actions/core";
+import { getSettings } from "./settings.ts";
 
 export async function run(): Promise<void> {
     try {
+        const settings = getSettings();
+        core.debug(`Settings: ${JSON.stringify(settings, null, 2)}`);
     } catch (error: unknown) {
         if (error instanceof Error) {
             core.setFailed(error.message);
