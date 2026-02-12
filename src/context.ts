4bc4d82cc6f5a9c4abc76048eed49ff1d08724a8
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-02-12 15:59:53 +0100

    feat(context): build context from webhook payload

--- src/context.ts
+++ src/context.ts
@@ -1,13 +1,41 @@
 import * as github from "@actions/github";
 import * as core from "@actions/core";
+import type { PullRequestEvent } from "@octokit/webhooks-types";
 import type { Context } from "./types";
 
 export function buildContext(): Context {
     core.info("Building context from payload...");
 
-    const payload = github.context.payload;
+    const payload = github.context.payload as PullRequestEvent;
+    const pr = payload.pull_request;
 
     core.debug(`Payload:\n${JSON.stringify(payload, null, 2)}`);
 
-    return {};
+    return {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+
+        repoUrl: pr.base.repo.html_url,
+        issueUrl: pr.issue_url,
+
+        number: pr.number,
+
+        title: pr.title,
+        body: pr.body ?? "",
+
+        baseBranch: pr.base.ref,
+        headBranch: pr.head.ref,
+
+        userLogin: pr.user.login,
+        authorAssociation: pr.author_association,
+
+        labels: pr.labels.map((label) => label.name),
+        milestone: pr.milestone?.title ?? null,
+
+        isDraft: pr.draft,
+        // maintainerCanModify: pr.maintainer_can_modify,
+        // changedFiles: pr.changed_files,
+        // additions: pr.additions,
+        // deletions: pr.deletions,
+    };
 }
