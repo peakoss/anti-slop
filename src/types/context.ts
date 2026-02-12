f8f0f1250c0f8409bc704ab990cd87455a3a8248
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-02-12 15:58:48 +0100

    feat(context): add used payload parameters to context type

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,1 +1,29 @@
-export interface Context {}
+import type { AuthorAssociation } from "@octokit/webhooks-types";
+
+export interface Context {
+    owner: string;
+    repo: string;
+
+    repoUrl: string;
+    issueUrl: string;
+
+    number: number;
+
+    title: string;
+    body: string;
+
+    baseBranch: string;
+    headBranch: string;
+
+    userLogin: string;
+    authorAssociation: AuthorAssociation;
+
+    labels: string[];
+    milestone: string | null;
+
+    isDraft: boolean;
+    // maintainerCanModify: boolean;
+    // changedFiles: number;
+    // additions: number;
+    // deletions: number;
+}
