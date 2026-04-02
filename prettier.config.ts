0dac1e82a4542ee952373265daa570509b79c443
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-04-02 17:07:25 +0200

    chore(tooling): switch indentation from spaces to tabs

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,8 +1,8 @@
 import { type Config } from "prettier";
 
 const config: Config = {
-    printWidth: 100,
-    useTabs: false,
+    printWidth: 100, // Matches Rust default and looks the best
+    useTabs: true, // Preferred for accessibility (screen readers, variable fonts) and tab width is easily adjustable per user
     plugins: ["@ianvs/prettier-plugin-sort-imports"],
     importOrderTypeScriptVersion: "5.0.0",
     importOrder: [
