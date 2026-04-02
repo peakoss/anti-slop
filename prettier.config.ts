2b7c1c7597272c10add4a998e772a53d061ad78c
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2026-04-02 17:02:10 +0200

    chore(tooling): add import sorting to prettier

--- prettier.config.ts
+++ prettier.config.ts
@@ -3,6 +3,17 @@ import { type Config } from "prettier";
 const config: Config = {
     printWidth: 100,
     useTabs: false,
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
+    importOrderTypeScriptVersion: "5.0.0",
+    importOrder: [
+        "<TYPES>^(node:)",
+        "<TYPES>",
+        "<TYPES>^[.]",
+        "<BUILTIN_MODULES>",
+        "<THIRD_PARTY_MODULES>",
+        "^#.+",
+        "^[.]",
+    ],
 };
 
 export default config;
